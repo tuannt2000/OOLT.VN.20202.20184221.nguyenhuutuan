c221836d675567810952e075e6349046d25de321
tuannt2000 <79615528+tuannt2000@users.noreply.github.com>
2021-03-04 13:22:31 +0700

    Create testbai4.java

---
+++ week2/testbai4.java
@@ -0,0 +1,15 @@
+package week2;
+import java.util.*;
+
+public class testbai4 {
+    public static void main (String[] args){
+        bai4 b4=new bai4();
+        b4.nhap();
+        b4.nhapmt1();
+        b4.nhapmt2();
+        System.out.println("ma tran 1:");
+        b4.xuatmt1();
+        System.out.println("ma tran 2:");
+        b4.xuatmt2();
+    }
+}
