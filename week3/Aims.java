4a4a1b9fd64cb8c63f10d4aa154aa32e8cd1152e
tuannt2000 <79615528+tuannt2000@users.noreply.github.com>
2021-03-11 18:32:54 +0700

    Create Aims.java

---
+++ week3/Aims.java
@@ -0,0 +1,21 @@
+package week3;
+
+public class Aims {
+    public static void main (String[] args){
+        Order anOrder = new Order() ;
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King","Animation","Roger Allers",87,19.95f);
+        anOrder.addDigitalVideoDisc(dvd1);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars","Science Fiction","George Lucas",87,24.95f);
+        anOrder.addDigitalVideoDisc(dvd2);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin","Animation",18.99f);
+        anOrder.addDigitalVideoDisc(dvd3);
+        DigitalVideoDisc dvd4 = new DigitalVideoDisc("Aladin","Animation",18.99f);
+        anOrder.addDigitalVideoDisc(dvd4);
+
+        System.out.print("Total Cost is: ");
+        System.out.println(anOrder.totalCost());
+        anOrder.removeDigitalVideoDisc(dvd4);
+        System.out.print("Total Cost is: ");
+        System.out.println(anOrder.totalCost());
+    }
+}
