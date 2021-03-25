7d0c07a45a1b5a283d1405c3a70b96884735bbb6
tuannt2000 <79615528+tuannt2000@users.noreply.github.com>
2021-03-25 17:45:14 +0700

    Create testbai3.java

---
+++ java/OtherProjects/Hust/Soict/Hedspi/lap02/src/testbai3.java
@@ -0,0 +1,12 @@
+public class testbai3 {
+    public static void main (String[] args){
+        bai3 b3= new bai3();
+        b3.nhap();
+        b3.nhapmang();
+        b3.sapxep();
+        System.out.println("sắp xếp từ bé đến lớn");
+        b3.xuatmang();
+        b3.tong();
+        b3.tb();
+    }
+}
