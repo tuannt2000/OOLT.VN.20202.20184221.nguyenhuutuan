0f173770636e741de14c5f56b3c77c1c36303478
tuannt2000 <79615528+tuannt2000@users.noreply.github.com>
2021-03-30 15:12:59 +0700

    Update NoGarbage.java

--- java/OtherProjects/Hust/Soict/Hedspi/garbage/src/NoGarbage.java
+++ java/OtherProjects/Hust/Soict/Hedspi/garbage/src/NoGarbage.java
@@ -1,3 +1,17 @@
+import java.io.*;
+import java.util.*;
+
 public class NoGarbage {
-    
+    public static void main(String[] args) throws IOException {
+        FileReader fr = new FileReader("C:\\Users\\Admin\\Documents\\java\\OtherProjects\\Hust\\Soict\\Hedspi\\garbage\\src\\text.txt");
+        Scanner sc = new Scanner(fr);
+        StringBuffer line = new StringBuffer();
+        while(sc.hasNext()){
+            line.append(sc.nextLine());
+            line.append("\n");
+        }
+        System.out.println(line);
+        // Close the file.
+        sc.close();
+    }
 }
