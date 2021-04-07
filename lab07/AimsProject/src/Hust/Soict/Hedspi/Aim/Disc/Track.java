b84a948b82c734a0cc174fe40ae6b379c7cd5275
tuannt2000 <79615528+tuannt2000@users.noreply.github.com>
2021-04-07 22:36:08 +0700

    Create Track.java

---
+++ lab07/AimsProject/src/Hust/Soict/Hedspi/Aim/Disc/Track.java
@@ -0,0 +1,24 @@
+package Disc;
+
+public class Track implements Playable{
+    private  String title;
+    private  int length;
+    
+    public String getTitle() {
+        return title;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public Track(String title, int length) {
+        this.title = title;
+        this.length = length;
+    }
+
+    public void play() {
+        System.out.println("Playing Track: " + this.getTitle());
+        System.out.println("Track length: " + this.getLength());
+    }
+}
