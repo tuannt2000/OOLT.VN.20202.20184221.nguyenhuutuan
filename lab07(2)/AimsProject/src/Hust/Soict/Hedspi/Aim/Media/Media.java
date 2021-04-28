4539d81c262055099951799239b5a06b00f6a678
tuannt2000 <79615528+tuannt2000@users.noreply.github.com>
2021-04-28 19:59:43 +0700

    Create Media.java

---
+++ lab07(2)/AimsProject/src/Hust/Soict/Hedspi/Aim/Media/Media.java
@@ -0,0 +1,35 @@
+package Media;
+
+public abstract class Media {
+    protected String title;
+    protected String category;
+    protected float cost;
+    protected int id;
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public float getCost() {
+        return cost;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public abstract String showInfo();
+}
