7d358d292753acd09a261214c04bc8c7c787b9de
tuannt2000 <79615528+tuannt2000@users.noreply.github.com>
2021-05-15 15:36:34 +0700

    Create DateTest.java

---
+++ lab10/AimsProject/src/Hust/Soict/Hedspi/Test/Utils/DateTest.java
@@ -0,0 +1,12 @@
+package src.Hust.Soict.Hedspi.Test.Utils;
+
+import Order.Order;
+import Disc.*;
+import Utils.Mydate;
+
+public class DateTest {
+    public static void main(String[] args) {
+        Mydate date = new Mydate(null, null, null);
+
+    }
+}
